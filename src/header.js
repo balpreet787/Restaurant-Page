57bcc3e639343c4014f26ecd2bbbac2c41243734
balpreet787 <balpreetsekhon787@gmail.com>
2022-10-06 11:30:44 -0700

    Add basic code and use webpack

---
+++ src/header.js
@@ -0,0 +1,21 @@
+const header = () =>{
+    const content = document.querySelector(".content");
+    const headerDiv = document.createElement("div");
+    headerDiv.classList.add("header");
+
+    const mainBtn = document.createElement("button");
+    mainBtn.classList.add("btn", "main");
+    mainBtn.textContent = "Main"
+    const menuBtn = document.createElement("button");
+    menuBtn.classList.add("btn", "menu");
+    menuBtn.textContent = "Menu"
+    const contactBtn = document.createElement("button");
+    contactBtn.classList.add("btn", "contact");
+    contactBtn.textContent = "Contact Us"
+
+    headerDiv.appendChild(mainBtn);
+    headerDiv.appendChild(menuBtn);
+    headerDiv.appendChild(contactBtn);
+    content.appendChild(headerDiv);
+};
+export default header;
